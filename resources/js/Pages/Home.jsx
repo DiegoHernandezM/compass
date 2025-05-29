bed57d701dff9c8cd8e7eabe0907962b17c1fb2b
Diego Hernandez <diegohernandez@Diegos-Mac-mini.local>
2025-05-28 19:23:24 -0600

    first commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className="p-6 text-gray-900">
+        <h1 className="text-2xl font-bold">Bienvenido a tu app Laravel + React + Inertia</h1>
+      </div>
+    )
+  }
+  
