8d5cd0197f6a789e4c7e87f388978863fc40b675
Diego Hernandez <dhernandezm@iMac-de-Diego.local>
2025-05-29 09:48:12 -0600

    cambio a react

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
-    return (
-      <div className="p-6 text-gray-900">
-        <h1 className="text-2xl font-bold">Bienvenido a tu app Laravel + React + Inertia</h1>
-      </div>
-    )
-  }
-  
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-3xl font-bold text-blue-700">¡Hola desde Inertia + React!</h1>
+      <p className="mt-4">Tu configuración ya está funcionando correctamente 🎉</p>
+    </div>
+  );
+}
