49ff517da05243afa71e3960881611b74f174c18
Diego Hdez <tsu.diego.hdezm@gmail.com>
2025-08-15 19:39:42 -0600

    cambio de idioma

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -66,21 +66,6 @@ export default function Login({ status, canResetPassword }) {
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
-                <div className="mt-4 block">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) =>
-                                setData('remember', e.target.checked)
-                            }
-                        />
-                        <span className="ms-2 text-sm text-gray-600">
-                            Recordarme
-                        </span>
-                    </label>
-                </div>
-
                 <div className="mt-4 flex items-center justify-end">
                     {canResetPassword && (
                         <Link
