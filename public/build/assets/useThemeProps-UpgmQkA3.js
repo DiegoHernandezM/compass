923dc87b7495718d8ef88fbc79a4595d802032a2
Diego Hdez <tsu.diego.hdezm@gmail.com>
2025-09-27 13:51:09 -0600

    build

---
+++ public/build/assets/useThemeProps-UpgmQkA3.js
@@ -0,0 +1,1 @@
+import{af as a,V as m}from"./Modal-rIM6T3Aq.js";import{g as f}from"./getThemeProps-7Rpbd0ZB.js";const i=a();function n({props:t,name:r,defaultTheme:o,themeId:s}){let e=m(o);return s&&(e=e[s]||e),f({theme:e,name:r,props:t})}export{i as s,n as u};
